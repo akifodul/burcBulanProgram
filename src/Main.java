d51a7873a9dfc77d39c9a8e684189531847c2119
[akif ödül] <[akifodul@hotmail.com]>
2023-08-09 08:02:53 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,66 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int month,day;
+        String burc = "";
+        boolean isError = false;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Scanner input = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.print("Doğduğunuz ay: ");
+        month = input.nextInt();
+
+        System.out.print("Doğduğunuz gün: ");
+        day = input.nextInt();
+
+        switch (month) {
+            case 1:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22) {
+                        burc = "Oğlak";
+                    } else {
+                        burc = "Kova";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+
+
+            case 2:
+                if (day >= 1 && day <= 28) {
+                    if (day < 20) {
+                        burc = "Kova";
+                    } else {
+                        burc = "Balık";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+
+            case 3:
+                if (day >= 1 && day <= 31) {
+                    if (day < 21) {
+                        burc = "Balık";
+                    } else {
+                        burc = "Koç";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            default:
+                System.out.print("Hatalı bir ay girdiniz!");
         }
+
+        if (isError) {
+            System.out.print("Hatalı giriş yaptınız, tekrar deneyiniz.");
+        } else {
+            System.out.print("Burcunuz: " + burc);
+        }
+
+
     }
 }
